671beee39d452d8b3a92315cb8d570f6f574ef24
William Owen <william.owen.bsc@gmail.com>
2022-08-13 12:45:58 -0700

    d3 SVG in place for intro

--- src/components/ux/WebsiteLoading.js
+++ src/components/ux/WebsiteLoading.js
@@ -1,7 +1,8 @@
 import gsap from '../../../gsap/gsap.min'
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useMemo, useRef } from 'react'
 import styled from 'styled-components'
 import WO from '../../layouts/components/WO'
+import * as d3 from 'd3'
 
 export default function WebsiteLoading() {
 
@@ -21,15 +22,48 @@ export default function WebsiteLoading() {
     }
 
 
-    apply().then(() => {
-      comp.current.remove()
-    })
+    // apply().then(() => {
+    //   comp.current.remove()
+    // })
   }, [])
 
   return (
-    <WebsiteLoadingStyled ref={comp}><WO /></WebsiteLoadingStyled>
+    <WebsiteLoadingStyled ref={comp}><Animation /></WebsiteLoadingStyled>
   )
 }
+
+const Animation = () => {
+  const wo = useRef()
+  useEffect(() => {
+
+
+    // alert('hi')
+    const ref = d3.select(".animation_container")
+      .append("svg")
+      .attr('viewBox', '0 0 100 100')
+      .attr('position', 'absolute')
+      .attr('top', 0)
+      .append('circle')
+      .attr('fill', 'blue')
+      .attr('cx', 40)
+      .attr('cy', 50)
+      .attr('r', 40);
+
+    return () => {
+      ref.remove()
+    }
+
+  }, [])
+
+  // useEffect(() => {
+  // }, [wo.current])
+  return (
+    <span className='animation_container' >
+      <WO />
+    </span>
+  )
+}
+
 
 const WebsiteLoadingStyled = styled.div`
   position: fixed;
@@ -42,4 +76,13 @@ const WebsiteLoadingStyled = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  .animation_container {
+    svg{
+
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+    /* width: 100vw; */
+  }
 `
