55ce80b7058a403a8146903467bd1ed72d39d014
William Owen <william.owen.bsc@gmail.com>
2022-08-24 19:51:38 -0700

    styles

--- src/context/store.js
+++ src/context/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit'
-import appReducer from './appReducer.js'
+import { configureStore } from "@reduxjs/toolkit";
+import appReducer from "./appReducer.js";
 
 export const store = configureStore({
   reducer: {
-    app: appReducer
-  }
-})
+    app: appReducer,
+  },
+});
--- src/layouts/header/context/store.js
+++ src/layouts/header/context/store.js
@@ -1,10 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit'
-import navbarReducer from './navbarReducer.js'
-import appReducer from '../../../context/appReducer.js'
+import { configureStore } from "@reduxjs/toolkit";
+import navbarReducer from "./navbarReducer.js";
+import appReducer from "../../../context/appReducer.js";
 
 export const store = configureStore({
   reducer: {
     navbar: navbarReducer,
-    app: appReducer
-  }
-})
+    app: appReducer,
+  },
+});
