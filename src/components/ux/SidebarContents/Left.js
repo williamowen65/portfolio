26ddbfc21bb031f0c846a43763f7e3757e5b360f
William Owen <william.owen.bsc@gmail.com>
2022-09-02 09:01:47 -0700

    left icon hover

--- src/components/ux/SidebarContents/Left.js
+++ src/components/ux/SidebarContents/Left.js
@@ -7,16 +7,20 @@ import {
 import { FiCodepen } from "react-icons/fi";
 import configValues from "../../../data/configValues.json";
 import SidebarSVG from "./SidebarSVG";
+import "./styles/index.css";
+
+const icons = "iconsSVG";
 
 export default function Left() {
   return (
-    <>
+    <LeftStyled>
       <BsMedium
         size={30}
         color={
           configValues.theme.dark.color
             .main
         }
+        className={icons}
       />
       <BsGithub
         size={30}
@@ -24,6 +28,7 @@ export default function Left() {
           configValues.theme.dark.color
             .main
         }
+        className={icons}
       />
       <FiCodepen
         size={30}
@@ -31,10 +36,29 @@ export default function Left() {
           configValues.theme.dark.color
             .main
         }
+        className={icons + " stroke"}
       />
       <SidebarSVG />
-    </>
+    </LeftStyled>
   );
 }
 
-const LeftStyled = styled.div``;
+const LeftStyled = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  svg.iconsSVG {
+    transition: all 0.5s;
+    &:hover {
+      &:not(.stroke) {
+        fill: ${configValues.theme.dark
+          .color.highlight};
+      }
+      &.stroke {
+        stroke: ${configValues.theme
+          .dark.color.highlight};
+      }
+      transform: translateY(-3px);
+    }
+  }
+`;
