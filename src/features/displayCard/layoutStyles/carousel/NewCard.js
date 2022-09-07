3f299eb7f51733326e29d280c94a5a420a6ef94a
William Owen <william.owen.bsc@gmail.com>
2022-09-07 10:16:45 -0700

    styles for cards

--- src/features/displayCard/layoutStyles/carousel/NewCard.js
+++ src/features/displayCard/layoutStyles/carousel/NewCard.js
@@ -1,11 +1,19 @@
 import React from "react";
 import styled from "styled-components";
 import { BiArrowFromLeft } from "react-icons/bi";
-import borderImg from "./assets/borderPattern.png";
+import { HiArrowNarrowRight } from "react-icons/hi";
+// import borderImg from "./assets/borderPattern.png";
+// import borderImg2 from "./assets/borderPattern2.png";
+// import borderImg2p1 from "./assets/borderPattern2.1.png";
+import CornerArt from "./assets/cornerArt.png";
+import CornerArt2 from "./assets/cornerArt2.png";
 
 export default function Card() {
   const kayakPic =
     "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/241370100_10223121600261531_6198239796508220196_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=M_GbXTiAph4AX9J9wMK&_nc_ht=scontent-sea1-1.xx&oh=00_AT-_BMPLZDpX4cTrqOJm3BfrqHn3zbYpzFkGaBVC0SM-Vw&oe=631DCDE0";
+
+  const webDevPic =
+    "https://scontent-sea1-1.xx.fbcdn.net/v/t39.30808-6/302085057_10225037046066479_347752878933419272_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=ICVasttexKYAX8P4IM4&_nc_ht=scontent-sea1-1.xx&oh=00_AT_D84gdgQS9kdNuBTuwbWDqtJMvb-gO-XIGHhMJtk6PKg&oe=631E7305";
 
   return (
     <CardStyled>
@@ -25,12 +33,27 @@ export default function Card() {
             From Kayak Guide to Web
             Developer
           </h3>
-          <img
-            className="coverPhoto"
-            src={kayakPic}
-            alt=""
-          />
+          <div className="content">
+            <div className="imgContainer">
+              <img
+                className="coverPhoto"
+                src={kayakPic}
+                alt=""
+              />
+            </div>
+            <HiArrowNarrowRight
+              size={30}
+            />
+            <div className="imgContainer">
+              <img
+                className="coverPhoto"
+                src={webDevPic}
+                alt=""
+              />
+            </div>
+          </div>
           <p>dfhsljdflsdds</p>
+          <button>Learn More</button>
         </div>
         <div className="slide two">
           <p>hsdfhsdlfhsdklhf</p>
@@ -52,8 +75,9 @@ export default function Card() {
 const CardStyled = styled.div`
   display: flex;
   .card {
-    /* border: 30px solid transparent; */
-    border-image: url(${borderImg}) 3000
-      round !important;
+    &:not(.intro) {
+      background-image: url(${CornerArt}),
+        url(${CornerArt2}) !important;
+    }
   }
 `;
