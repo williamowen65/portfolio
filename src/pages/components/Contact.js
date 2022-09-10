316ec0f0567cfed826e49b2e746d257aae91a332
William Owen <william.owen.bsc@gmail.com>
2022-09-10 13:20:07 -0700

    Revert "got rid of pre styled buttons... making my own for consistency"
    This reverts commit 0a5fd47b99dbd2fc42b8e20b939ab0cf78c0f824.

--- src/pages/components/Contact.js
+++ src/pages/components/Contact.js
@@ -8,11 +8,6 @@ import {
 import configValues from "../../data/configValues.json";
 import QR from "../../assets/qrcode_www.wowebdev.com.png";
 import contactCard from "../../assets/card.png";
-import LinkedInImg from "../../assets/Linkedin.png";
-import {
-  SpeedDial,
-  SpeedDialAction,
-} from "@mui/material";
 
 export default function Contact() {
   return (
@@ -38,55 +33,19 @@ export default function Contact() {
                 .color.main
             }
           /> */}
-
-          <SpeedDial
-            ariaLabel="SpeedDial basic example"
-            sx={{
-              position: "absolute",
-              bottom: -16,
-              right: 0,
+          <iframe
+            src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fwww.WOWebDev.com&layout=button&size=small&width=77&height=20&appId"
+            width="77"
+            height="20"
+            style={{
+              border: "none",
+              overflow: "hidden",
             }}
-            direction="left"
-            icon={
-              <BsShareFill
-                color={
-                  configValues.theme
-                    .dark.color.main
-                }
-                size={24}
-              />
-            }
-          >
-            {/* {actions.map((action) => ( */}
-            <SpeedDialAction
-              icon={<BsLinkedin />}
-              onClick={(e) => {
-                e.preventDefault();
-                window.open(
-                  "http://www.linkedin.com/shareArticle?mini=true&url=http://www.wowebdev.com/",
-                  "linkedin",
-                  "left=20,top=20,width=500,height=500,toolbar=1,resizable=0"
-                );
-                return false;
-              }}
-              tooltipTitle="LinkedIn"
-            />
-            <SpeedDialAction
-              icon={<BsFacebook />}
-              onClick={(e) => {
-                e.preventDefault();
-                window.open(
-                  "https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&u=http%3A%2F%2Fwww.WOWebDev.com%2F&ref=plugin&src=share_button",
-                  "facebook",
-                  "left=20,top=20,width=500,height=500,toolbar=1,resizable=0"
-                );
-                return false;
-              }}
-              tooltipTitle="Facebook"
-            />
-
-            {/* ))} */}
-          </SpeedDial>
+            scrolling="no"
+            frameBorder="0"
+            allowFullScreen
+            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
+          ></iframe>
         </span>
       </header>
 
@@ -140,19 +99,11 @@ export default function Contact() {
 }
 
 const ContactStyled = styled.div`
-  a {
-    display: block;
-  }
   img {
     box-sizing: content-box;
     width: 375px;
     max-width: 100%;
     margin: 0 auto;
-    &.linkedin {
-      width: 74px;
-      background: #2b7ab8;
-      /* border */
-    }
   }
   header {
     h2 {
@@ -162,6 +113,17 @@ const ContactStyled = styled.div`
   .share {
     position: relative;
     &:hover {
+      /* background: ${configValues
+        .theme.dark.color.mainOffset +
+      "aa"}; */
+      /* 
+      svg {
+        stroke: ${configValues.theme
+        .dark.background.color.window};
+        fill: ${configValues.theme.dark
+        .background.color.window};
+      } */
+      /* border-radius: 50%; */
       cursor: pointer;
     }
   }
