0a5fd47b99dbd2fc42b8e20b939ab0cf78c0f824
William Owen <william.owen.bsc@gmail.com>
2022-09-09 17:25:18 -0700

    got rid of pre styled buttons... making my own for consistency

--- src/pages/components/Contact.js
+++ src/pages/components/Contact.js
@@ -4,6 +4,7 @@ import { BsShareFill } from "react-icons/bs";
 import configValues from "../../data/configValues.json";
 import QR from "../../assets/qrcode_www.wowebdev.com.png";
 import contactCard from "../../assets/card.png";
+import LinkedInImg from "../../assets/Linkedin.png";
 
 export default function Contact() {
   return (
@@ -29,19 +30,36 @@ export default function Contact() {
                 .color.main
             }
           /> */}
-          <iframe
-            src="https://www.facebook.com/plugins/share_button.php?href=http%3A%2F%2Fwww.WOWebDev.com&layout=button&size=small&width=77&height=20&appId"
-            width="77"
-            height="20"
-            style={{
-              border: "none",
-              overflow: "hidden",
+
+          <a
+            href="https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&u=http%3A%2F%2Fwww.WOWebDev.com%2F&display=popup&ref=plugin&src=share_button"
+            onClick={(e) => {
+              e.preventDefault();
+              window.open(
+                "https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&u=http%3A%2F%2Fwww.WOWebDev.com%2F&ref=plugin&src=share_button",
+                "facebook",
+                "left=20,top=20,width=500,height=500,toolbar=1,resizable=0"
+              );
+              return false;
             }}
-            scrolling="no"
-            frameBorder="0"
-            allowFullScreen
-            allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"
-          ></iframe>
+          >
+            FACEBOOK
+          </a>
+
+          <a
+            href="http://www.linkedin.com/shareArticle?mini=true&url=http://www.wowebdev.com/"
+            onClick={(e) => {
+              e.preventDefault();
+              window.open(
+                "http://www.linkedin.com/shareArticle?mini=true&url=http://www.wowebdev.com/",
+                "linkedin",
+                "left=20,top=20,width=500,height=500,toolbar=1,resizable=0"
+              );
+              return false;
+            }}
+          >
+            LINKED IN
+          </a>
         </span>
       </header>
 
@@ -95,11 +113,19 @@ export default function Contact() {
 }
 
 const ContactStyled = styled.div`
+a {
+  display: block;
+}
   img {
     box-sizing: content-box;
     width: 375px;
     max-width: 100%;
     margin: 0 auto;
+    &.linkedin {
+      width: 74px;
+      background: #2b7ab8;
+      border
+    }
   }
   header {
     h2 {
@@ -114,15 +140,20 @@ const ContactStyled = styled.div`
       transform: translateX(2px);
     } */
     &:hover {
-      /* background: ${configValues
-        .theme.dark.color.mainOffset +
-      "aa"}; */
+      /* background: ${
+        configValues.theme.dark.color
+          .mainOffset + "aa"
+      }; */
       /* 
       svg {
-        stroke: ${configValues.theme
-        .dark.background.color.window};
-        fill: ${configValues.theme.dark
-        .background.color.window};
+        stroke: ${
+          configValues.theme.dark
+            .background.color.window
+        };
+        fill: ${
+          configValues.theme.dark
+            .background.color.window
+        };
       } */
       /* border-radius: 50%; */
       cursor: pointer;
